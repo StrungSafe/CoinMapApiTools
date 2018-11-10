02e6dc262c111f43a81dd424388f85332efb65b4
Miguel Molina Jr <mmolina@foldingcoin.net>
2018-11-10 11:08:26 -0500

    Print venue details to an output file

--- CoinMapApiTools/CoinMapApiTools.Console/Program.cs
+++ CoinMapApiTools/CoinMapApiTools.Console/Program.cs
@@ -1,6 +1,7 @@
 ﻿namespace CoinMapApiTools.Console
 {
     using System;
+    using System.Collections.Generic;
     using System.Configuration;
     using System.IO;
     using System.Net;
@@ -17,20 +18,68 @@
         public void Run()
         {
             VenueList venues = GetVenues();
+
+            var venuesWithEmail = new List<Venue>();
+
+            foreach (Venue venue in venues.Venues)
+            {
+                Venue venueDetail = GetVenue(venue.Id);
+
+                if (string.IsNullOrWhiteSpace(venueDetail.Email))
+                {
+                    continue;
+                }
+
+                venuesWithEmail.Add(venueDetail);
+            }
+
+            WriteVenuesToFile(venuesWithEmail.ToArray());
+        }
+
+        private Uri GetUri()
+        {
+            return new Uri(ConfigurationManager.AppSettings["uri"]);
+        }
+
+        private Venue GetVenue(int venueId)
+        {
+            var uri = new Uri(GetUri(), venueId.ToString());
+
+            string venue = HttpGet(uri);
+
+            return JsonConvert.DeserializeObject<Venue>(venue);
         }
 
         private VenueList GetVenues()
         {
-            WebRequest request = WebRequest.Create(ConfigurationManager.AppSettings["url"]);
+            string venues = HttpGet(GetUri());
+
+            return JsonConvert.DeserializeObject<VenueList>(venues);
+        }
+
+        private string HttpGet(Uri uri)
+        {
+            WebRequest request = WebRequest.Create(uri);
 
             using (WebResponse response = request.GetResponse())
             using (Stream responseStream = response.GetResponseStream())
             using (var streamReader =
                 new StreamReader(responseStream ?? throw new ArgumentNullException(nameof(responseStream))))
             {
-                string venues = streamReader.ReadToEnd();
-                return JsonConvert.DeserializeObject<VenueList>(venues);
+                return streamReader.ReadToEnd();
             }
+        }
+
+        private void WriteVenuesToFile(Venue[] venues)
+        {
+            string venuesCsv = string.Empty;
+
+            foreach (Venue venue in venues)
+            {
+                venuesCsv += $"{venue.Id},{venue.Name},{venue.Email},{venue.Country}{Environment.NewLine}";
+            }
+
+            File.WriteAllText(ConfigurationManager.AppSettings["output"], venuesCsv);
         }
     }
 }
