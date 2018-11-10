7e4a28175d542cfb59f6e2a8a62ecc6271ccc3c6
Miguel Molina Jr <mmolina@foldingcoin.net>
2018-11-10 10:29:49 -0500

    Download list of venues from coin map

---
+++ CoinMapApiTools/CoinMapApiTools.Console/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Net;
+
+namespace CoinMapApiTools.Console
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var program = new Program();
+            program.Run();
+        }
+
+        public void Run()
+        {
+            var venues = GetVenues();
+        }
+
+        private string GetVenues()
+        {
+            var request = WebRequest.Create(ConfigurationManager.AppSettings["url"]);
+
+            using (var response = request.GetResponse())
+            using (var responseStream = response.GetResponseStream())
+            using (var streamReader = new StreamReader(responseStream ?? throw new ArgumentNullException(nameof(responseStream))))
+            {
+                return streamReader.ReadToEnd();
+            }
+        }
+    }
+}
